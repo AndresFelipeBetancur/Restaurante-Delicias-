6a49b7b7d1335dff41baa92802397503fe9ac8ef
AndresFelipeBetancur <abetancurquintero066@gmail.com>
2026-04-07 16:52:44 -0500

    Agregué sistema de pedidos

---
+++ CarpetaCodeBlocks/main.cpp
@@ -0,0 +1,45 @@
+//Proyecto Restaurante Programacion Orientada a Objetos
+//Autores:
+//- Andres Felipe Betancur Quintero-202563100
+//- Juan Jose Garces Casas-
+//- Juan Manuel Carvajal-
+//- Eduardo Jose Castro-
+//- Nicolas Diaz Galeano-
+//Version: 1.0
+//Descripcion: Este proyecto es un sistema de gestion de restaurante,
+// el cual permite a los usuarios realizar pedidos,
+// a los empleados gestionar los pedidos y a los administradores gestionar el menu y las mesas.
+// Se utilizó la librería cpp-httplib para implementar un servidor HTTP en C++,
+// permitiendo la comunicación entre dispositivos móviles y el sistema del restaurante mediante solicitudes POST.
+// Mysql para la base de datos.
+
+
+#define _WIN32_WINNT 0x0A00
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include "httplib.h"-
+
+#include <iostream>
+#include "httplib.h"
+
+using namespace httplib;
+
+int main() {
+    Server svr;
+
+    // Ruta principal
+    svr.Get("/", [](const Request& req, Response& res) {
+        res.set_content("Servidor funcionando", "text/plain");
+    });
+
+    // Ruta pedidos
+    svr.Post("/pedido", [](const Request& req, Response& res) {
+        std::cout << "Pedido: " << req.body << std::endl;
+        res.set_content("OK", "text/plain");
+    });
+
+    svr.listen("0.0.0.0", 8080);
+
+    return 0;
+}
