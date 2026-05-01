35b7565619e40c4122d0a0159d9f50e273061137
JuanesG4 <juanjogarces12@gmail.com>
2026-05-01 16:35:54 -0500

    Hola soy Goku

---
+++ CarpetaCodeBlocks/include/archivos.h
@@ -0,0 +1,8 @@
+//Hola soy Goku, Felipe, mi amigo Juan me dijo que te dejara el siguiente mensaje:
+//"XD" atte:Juan
+//"Entonces en usuarios hago lo mismo?"
+#ifndef USUARIOS_H
+#define USUARIOS_H
+#include <iostream>
+#include <vector>
+using namespace std;
